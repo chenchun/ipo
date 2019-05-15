38ebd8fa517c93a275ab2bb01beb9dd545945c04
Chun Chen <chenchun.feed@gmail.com>
2019-05-15 23:28:19 +0800

    Re-Init module from dummy module

--- ipo.c
+++ ipo.c
@@ -1,14 +1,3 @@
-//
-// Created by ramichen on 19-4-19.
-//
-
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
-#define DRV_NAME	"ipo"
-#define DRV_VERSION	"1.0"
-#define DRV_DESCRIPTION	"IP option device driver"
-#define DRV_COPYRIGHT	"(C) 2019-2020 Chun Chen <ramichen@tencent.com>"
-
 #include <linux/module.h>	/* Needed by all modules */
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
@@ -17,17 +6,18 @@
 #include <linux/netdevice.h>
 #include <linux/u64_stats_sync.h>
 #include <linux/etherdevice.h>
+#include <linux/percpu-defs.h>
 
 #include <net/rtnetlink.h>
 #include <net/dst.h>
 #include <net/xfrm.h>
-#include <linux/percpu-defs.h>
 
-int tun_addr = 2;
+static int numipos = 1;
 
-struct ipo_priv {
-	atomic64_t dropped;
-};
+/* fake multicast ability */
+static void set_multicast_list(struct net_device *dev)
+{
+}
 
 struct pcpu_dstats {
 	u64			tx_packets;
@@ -35,79 +25,8 @@ struct pcpu_dstats {
 	struct u64_stats_sync	syncp;
 };
 
-static void ipo_get_drvinfo(struct net_device *dev,
-							  struct ethtool_drvinfo *info)
-{
-	strlcpy(info->driver, DRV_NAME, sizeof(info->driver));
-	strlcpy(info->version, DRV_VERSION, sizeof(info->version));
-}
-
-static int ipo_get_ts_info(struct net_device *dev,
-							 struct ethtool_ts_info *ts_info)
-{
-	ts_info->so_timestamping = SOF_TIMESTAMPING_TX_SOFTWARE |
-							   SOF_TIMESTAMPING_RX_SOFTWARE |
-							   SOF_TIMESTAMPING_SOFTWARE;
-
-	ts_info->phc_index = -1;
-
-	return 0;
-};
-
-static const struct ethtool_ops ipo_ethtool_ops = {
-	.get_drvinfo = ipo_get_drvinfo,
-	.get_ts_info = ipo_get_ts_info,
-};
-
-static int ipo_dev_init(struct net_device *dev)
-{
-//	struct ipo_priv *priv = netdev_priv(dev);
-
-	dev->dstats = netdev_alloc_pcpu_stats(struct pcpu_dstats);
-	if (!dev->dstats)
-		return -ENOMEM;
-
-//	dev->dev.parent = &ipo_parent;
-	return 0;
-}
-
-static void ipo_dev_uninit(struct net_device *dev)
-{
-	free_percpu(dev->dstats);
-}
-
-static netdev_tx_t ipo_xmit(struct sk_buff *skb, struct net_device *dev)
-{
-	struct pcpu_dstats *dstats = this_cpu_ptr(dev->dstats);
-
-	u64_stats_update_begin(&dstats->syncp);
-	dstats->tx_packets++;
-	dstats->tx_bytes += skb->len;
-	u64_stats_update_end(&dstats->syncp);
-
-	skb_tx_timestamp(skb);
-	dev_kfree_skb(skb);
-	struct iphdr *nh = (struct iphdr *)skb_network_header(skb);
-	printk(KERN_INFO "IPO ipo_xmit saddr %d, daddr %s\n", nh->saddr, nh->daddr);
-	return NETDEV_TX_OK;
-}
-
-// Check include/linux/netdevice.h for enum rx_handler_result
-static rx_handler_result_t ipo_rx(struct sk_buff **pskb)
-{
-	struct sk_buff *skb = *pskb;
-	struct iphdr *nh = (struct iphdr *)skb_network_header(skb);
-	printk(KERN_INFO "IPO ipo_rx saddr %d, daddr %s\n", nh->saddr, nh->daddr);
-	return RX_HANDLER_PASS;
-}
-
-/* fake multicast ability */
-static void set_multicast_list(struct net_device *dev)
-{
-}
-
 static void ipo_get_stats64(struct net_device *dev,
-							  struct rtnl_link_stats64 *stats)
+												   struct rtnl_link_stats64 *stats)
 {
 	int i;
 
@@ -125,6 +44,33 @@ static void ipo_get_stats64(struct net_device *dev,
 		stats->tx_bytes += tbytes;
 		stats->tx_packets += tpackets;
 	}
+}
+
+static netdev_tx_t ipo_xmit(struct sk_buff *skb, struct net_device *dev)
+{
+	struct pcpu_dstats *dstats = this_cpu_ptr(dev->dstats);
+
+	u64_stats_update_begin(&dstats->syncp);
+	dstats->tx_packets++;
+	dstats->tx_bytes += skb->len;
+	u64_stats_update_end(&dstats->syncp);
+
+	dev_kfree_skb(skb);
+	return NETDEV_TX_OK;
+}
+
+static int ipo_dev_init(struct net_device *dev)
+{
+	dev->dstats = alloc_percpu(struct pcpu_dstats);
+	if (!dev->dstats)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static void ipo_dev_uninit(struct net_device *dev)
+{
+	free_percpu(dev->dstats);
 }
 
 static int ipo_change_carrier(struct net_device *dev, bool new_carrier)
@@ -147,46 +93,25 @@ static const struct net_device_ops ipo_netdev_ops = {
 	.ndo_change_carrier	= ipo_change_carrier,
 };
 
-static int ipo_newlink(struct net *src_net, struct net_device *dev,
-						struct nlattr *tb[], struct nlattr *data[],
-						struct netlink_ext_ack *extack)
+static void ipo_setup(struct net_device *dev)
 {
-	int err;
-	if (!dev) {
-		printk(KERN_INFO "IPO ipo_newlink dev not nil\n");
-	}
-	if (tb[IFLA_ADDRESS] == NULL)
-		eth_hw_addr_random(dev);
+	ether_setup(dev);
 
-	if (tb[IFLA_IFNAME])
-		nla_strlcpy(dev->name, tb[IFLA_IFNAME], IFNAMSIZ);
-	else
-		snprintf(dev->name, IFNAMSIZ, DRV_NAME "%%d");
+	/* Initialize the device structure. */
+	dev->netdev_ops = &ipo_netdev_ops;
+	dev->destructor = free_netdev;
 
-	err = register_netdevice(dev);
-	if (err < 0)
-		goto err_register_dev;
-
-	netif_carrier_off(dev);
-err_register_dev:
-	/* nothing to do */
-	return err;
+	/* Fill in device structure with ethernet-generic values. */
+	dev->tx_queue_len = 0;
+	dev->flags |= IFF_NOARP;
+	dev->flags &= ~IFF_MULTICAST;
+	dev->priv_flags |= IFF_LIVE_ADDR_CHANGE;
+	dev->features	|= NETIF_F_SG | NETIF_F_FRAGLIST | NETIF_F_TSO;
+	dev->features	|= NETIF_F_HW_CSUM | NETIF_F_HIGHDMA | NETIF_F_LLTX;
+	eth_hw_addr_random(dev);
 }
 
-static void ipo_dellink(struct net_device *dev, struct list_head *head)
-{
-	unregister_netdevice_queue(dev, head);
-	return;
-}
-
-
-static void ipo_dev_free(struct net_device *dev)
-{
-	free_percpu(dev->vstats);
-}
-
-static int ipo_validate(struct nlattr *tb[], struct nlattr *data[],
-						 struct netlink_ext_ack *extack)
+static int ipo_validate(struct nlattr *tb[], struct nlattr *data[])
 {
 	if (tb[IFLA_ADDRESS]) {
 		if (nla_len(tb[IFLA_ADDRESS]) != ETH_ALEN)
@@ -197,75 +122,61 @@ static int ipo_validate(struct nlattr *tb[], struct nlattr *data[],
 	return 0;
 }
 
-static void ipo_setup(struct net_device *dev)
-{
-	ether_setup(dev);
-
-	dev->priv_flags &= ~IFF_TX_SKB_SHARING;
-	dev->priv_flags |= IFF_LIVE_ADDR_CHANGE;
-	dev->priv_flags |= IFF_NO_QUEUE;
-	dev->priv_flags |= IFF_PHONY_HEADROOM;
-
-	dev->netdev_ops = &ipo_netdev_ops;
-	dev->ethtool_ops = &ipo_ethtool_ops;
-	dev->features |= NETIF_F_LLTX;
-//	dev->features |= VETH_FEATURES;
-	dev->vlan_features = dev->features &
-						 ~(NETIF_F_HW_VLAN_CTAG_TX |
-						   NETIF_F_HW_VLAN_STAG_TX |
-						   NETIF_F_HW_VLAN_CTAG_RX |
-						   NETIF_F_HW_VLAN_STAG_RX);
-//	dev->needs_free_netdev = true;
-//	dev->priv_destructor = ipo_dev_free;
-//	dev->max_mtu = ETH_MAX_MTU;
-
-//	dev->hw_features = VETH_FEATURES;
-//	dev->hw_enc_features = VETH_FEATURES;
-	dev->mpls_features = NETIF_F_HW_CSUM | NETIF_F_GSO_SOFTWARE;
-	netdev_rx_handler_register(dev, ipo_rx, NULL);
-}
-
-static struct net *ipo_get_link_net(const struct net_device *dev)
-{
-	return dev_net(dev);
-}
-
-static struct rtnl_link_ops ipo_link_ops = {
-	.kind		= DRV_NAME,
-	.priv_size	= sizeof(struct ipo_priv),
+static struct rtnl_link_ops ipo_link_ops __read_mostly = {
+	.kind		= "ipo",
 	.setup		= ipo_setup,
 	.validate	= ipo_validate,
-	.newlink	= ipo_newlink,
-	.dellink	= ipo_dellink,
-//	.policy		= veth_policy,
-//	.maxtype	= VETH_INFO_MAX,
-	.get_link_net	= ipo_get_link_net,
 };
 
-static __init int ipo_init(void)
+/* Number of ipo devices to be set up by this module. */
+module_param(numipos, int, 0);
+MODULE_PARM_DESC(numipos, "Number of ipo pseudo devices");
+
+static int __init ipo_init_one(void)
 {
+	struct net_device *dev_ipo;
+	int err;
+
+	dev_ipo = alloc_netdev(0, "ipo%d", ipo_setup);
+	if (!dev_ipo)
+		return -ENOMEM;
+
+	dev_ipo->rtnl_link_ops = &ipo_link_ops;
+	err = register_netdevice(dev_ipo);
+	if (err < 0)
+		goto err;
+	return 0;
+
+	err:
+	free_netdev(dev_ipo);
+	return err;
+}
+
+static int __init ipo_init_module(void)
+{
+	int i, err = 0;
+
+	rtnl_lock();
+	err = __rtnl_link_register(&ipo_link_ops);
+
+	for (i = 0; i < numipos && !err; i++) {
+		err = ipo_init_one();
+		cond_resched();
+	}
+	if (err < 0)
+		__rtnl_link_unregister(&ipo_link_ops);
+	rtnl_unlock();
 	printk(KERN_INFO "IPO installed\n");
-	return rtnl_link_register(&ipo_link_ops);
+	return err;
 }
 
-static __exit void ipo_cleanup(void)
+static void __exit ipo_cleanup_module(void)
 {
-	printk(KERN_INFO "IPO uninstalled\n");
 	rtnl_link_unregister(&ipo_link_ops);
+	printk(KERN_INFO "IPO uninstalled\n");
 }
 
-module_init(ipo_init);
-module_exit(ipo_cleanup);
-module_param(tun_addr, int, 0644);
-MODULE_PARM_DESC(tun_addr, "An integer");
-
-MODULE_DESCRIPTION(DRV_DESCRIPTION);
-MODULE_AUTHOR(DRV_COPYRIGHT);
+module_init(ipo_init_module);
+module_exit(ipo_cleanup_module);
 MODULE_LICENSE("GPL");
-
-/*
- *  This module uses /dev/ipo.  The MODULE_SUPPORTED_DEVICE macro might
- *  be used in the future to help automatic configuration of modules, but is
- *  currently unused other than for documentation purposes.
- */
-MODULE_SUPPORTED_DEVICE("ipo");
+MODULE_ALIAS_RTNL_LINK("ipo");
