af2dcf8d209b84dc7dd10c5695bf18f11a22596f
Chun Chen <chenchun.feed@gmail.com>
2019-07-12 00:00:32 +0800

    add debug command

--- createcontainer.sh
+++ createcontainer.sh
@@ -1,6 +1,4 @@
-if [ "$(ip netns list | grep ctn)" = "ctn (id: 0)" ]; then
-    exit 0;
-fi
+ip netns del ctn
 ip=192.168.1.3
 if [ "$(hostname)" = "node1" ]; then
     ip=192.168.2.3
@@ -10,6 +8,7 @@ ip link add v1 type veth peer name v2
 ip link set v2 up
 ip link set v1 netns ctn
 ip netns exec ctn ip add add $ip/32 dev v1
+ip netns exec ctn ip link set v1 mtu 1496
 ip netns exec ctn ip link set v1 up
 ip netns exec ctn ip link set lo up
 ip netns exec ctn ip r add 169.254.0.1 dev v1 scope link
