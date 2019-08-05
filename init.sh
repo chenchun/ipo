bed8dfcfcc4c220c799106f1e480cd8257fa8629
Chun Chen <chenchun.feed@gmail.com>
2019-08-05 21:06:33 +0800

    Fix init.sh

--- init.sh
+++ init.sh
@@ -26,13 +26,13 @@ ip link add ipo0 type ipo
 if [ $is_node1 -eq 1 ]; then
     ip ad add 192.168.1.2/24 dev ipo0
     ip link set ipo0 up
-    #ip route add 192.168.2.0/24 via 10.0.0.3 dev eth1 || true
-    ip r add 192.168.2.0/24 via 10.0.0.3 dev ipo0 onlink || true
+    #ip route add 192.168.2.0/24 via $node2 dev eth1 || true
+    ip r add 192.168.2.0/24 via $node2 dev ipo0 onlink || true
 else
     ip ad add 192.168.2.2/24 dev ipo0
     ip link set ipo0 up
-    #ip route add 192.168.1.0/24 via 10.0.0.2 dev eth1 || true
-    ip r add 192.168.1.0/24 via 10.0.0.2 dev ipo0 onlink || true
+    #ip route add 192.168.1.0/24 via $node1 dev eth1 || true
+    ip r add 192.168.1.0/24 via $node1 dev ipo0 onlink || true
 fi
 
 sysctl -w net.ipv4.ip_forward=1
